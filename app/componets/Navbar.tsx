970a619e5ab61f7411040197f770df8f0d5e6af4
EgecTech <egectech@gmail.com>
2025-06-10 23:06:16 +0300

    add nav sign in

--- app/componets/Navbar.tsx
+++ app/componets/Navbar.tsx
@@ -1,17 +1,44 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth;
   return (
-    <header className='px-5 py-3 by-white shadow-sm font-work-sans'>
-      <nav className='flex justify-between items-center'>
-        <Link href='/'><Image src="/logo.png" width={144} height={30} alt={'Logo '}/></Link>
-      </nav>
-      
-    </header>
-  )
-}
+    <header className="px-5 py-3 by-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" width={144} height={30} alt={"Logo "} />
+        </Link>
 
-export default Navbar
+        <div className="flex items-center gap-5">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <button onClick={signOut}>
+                <span>LogOut</span>
+              </button>
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
--- auth.ts
+++ auth.ts
@@ -1,7 +1,24 @@
-
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
-})
+
+const nextAuthOptions = {
+  providers: [GitHub({
+    clientId: process.env.GITHUB_ID!,
+    clientSecret: process.env.GITHUB_SECRET!,
+  })],
+};
+
+export const {
+  handlers,
+  signIn: nextAuthSignIn,
+  signOut: nextAuthSignOut,
+  auth,
+} = NextAuth(nextAuthOptions);
+
+export async function signIn(...args: Parameters<typeof nextAuthSignIn>) {
+  return await nextAuthSignIn(...args);
+}
+
+export async function signOut(...args: Parameters<typeof nextAuthSignOut>) {
+  return await nextAuthSignOut(...args);
+}
